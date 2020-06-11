99cff815073facf15de480c7fc0e2760c64c1313
Sven Giermann <sven.giermann@gmail.com>
2020-06-11 10:40:21 +0200

    Add test for Hysen

--- test/test.js
+++ test/test.js
@@ -5,26 +5,45 @@ let fs = require('fs');
 var b = new broadlink();
 
 b.on("deviceReady", (dev) => {
-    var timer = setInterval(function(){
-        console.log("send check!");
-        dev.checkData();
-    }, 1000);
+    var devType = dev.getType();
+    if (devType() == "RM2") {
+        var timer = setInterval(function(){
+            console.log("send check!");
+            dev.checkData();
+        }, 1000);
 
-    dev.on("temperature", (temp)=>{
-        console.log("get temp "+temp);
-        dev.enterLearning();
-    });
+        dev.on("temperature", (temp)=>{
+            console.log("get temp "+temp);
+            dev.enterLearning();
+        });
 
-    dev.on("rawData", (data) => {
-        fs.writeFile("test1", data, function(err) {
-            if(err) {
-                return console.log(err);
-            }
-            console.log("The file was saved!");
-            clearInterval(timer);
-        }); 
-    });
-    dev.checkTemperature();
+        dev.on("rawData", (data) => {
+            fs.writeFile("test1", data, function(err) {
+                if(err) {
+                    return console.log(err);
+                }
+                console.log("The file was saved!");
+                clearInterval(timer);
+            }); 
+        });
+        dev.checkTemperature();
+    } else if (devType() == "Hysen heating controller") {
+        dev.on("status", function(status) {
+            console.log(JSON.stringify(status));
+        });
+
+        dev.on("fullstatus", function(fullstatus) {
+            console.log(JSON.stringify(fullstatus));
+        });
+
+        dev.getFullStatus();
+        setInterval(function(){
+            dev.checkTemperature();
+        }, 1000);
+    } else {
+        console.log("Not a supported device yet.")
+        //setTimeout(function(){ process.exit(-1); }, 2000);
+    }
 
 });
 
